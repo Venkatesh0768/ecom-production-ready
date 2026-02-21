dcfbd2a5d45e15669796880d72705546d9dc45a0
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-02-21 18:35:08 +0530

    backend spring boot setup

---
+++ ecom-backend/src/test/java/org/ecom/ecombackend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecom.ecombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
